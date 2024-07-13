ddb77bc1c931a777f57dab00beb8229aa40c83e4
joralsiz95 <71951966+joralsiz95@users.noreply.github.com>
2024-07-13 07:23:36 -0500

    Actualizando dependencias

---
+++ src/main/java/com/example/proyectoForoHub/ProyectoForoHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectoForoHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoForoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoForoHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyectoForoHub/ProyectoForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoForoHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
